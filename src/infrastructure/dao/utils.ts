37f239bcd25104de86fd69858e7c26da0ec836b3
Matteo Gobbo <info@matteogobbo.it>
2024-08-16 18:54:37 +0200

    fix buildOrderBy

--- src/infrastructure/dao/utils.ts
+++ src/infrastructure/dao/utils.ts
@@ -3,6 +3,6 @@ import { SortBy } from "../../application/commons/models";
 export const buildOrderBy = <T>(sortBy: SortBy<T>) => {
   const split = sortBy.split(".");
   return {
-    [split[0]]: [split[1]],
+    [split[0]]: split[1],
   };
 };
